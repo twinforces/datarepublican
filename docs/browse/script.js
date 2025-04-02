16fc2fa991bdc7c197091bafbe88a2dd5f54bf5e
Pierce Wetter <pierce@twinforces.com>
2025-04-02 14:46:05 -0700

    Conflict appeared in preview build.

--- docs/browse/script.js
+++ docs/browse/script.js
@@ -1353,13 +1353,7 @@ const extraStyle = `
   .node.no-grants { cursor: zoom-out; }
   .link { stroke-opacity: 0.5; }
   .hat-up, .hat-down { cursor: pointer; }
-<<<<<<< HEAD
   text { fill: #000; }
-=======
-  #graph { background: #fff !important; }
-  text { fill: #000; }
-  svg { background: #fff !important; }
->>>>>>> dc58c1d4a6583d0ffb9b189ff6342c864d62e1a8
   .selected { stroke: #ff0; stroke-width: 2px; }
 `;
 d3.select("head").append("style").text(extraStyle);
