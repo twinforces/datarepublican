1b44a59e7402851e092f540b046f2b4c304da1f7
Pierce Wetter <pierce@twinforces.com>
2025-05-22 12:07:53 -0700

    remove duplicate xpaths.

--- charity/xpaths_990.py
+++ charity/xpaths_990.py
@@ -1,19 +1,19 @@
 XPATHS_990 = {
     "form_type": [
         ".//irs:ReturnHeader/irs:ReturnTypeCd",
-        ".//ReturnHeader/ReturnTypeCd"
+        ".//ReturnHeader/ReturnTypeCd",
     ],
     "tax_year": [
         ".//irs:ReturnHeader/irs:TaxYr",
-        ".//ReturnHeader/TaxYr"
+        ".//ReturnHeader/TaxYr",
     ],
     "filer_ein": [
         ".//irs:Filer/irs:EIN",
-        ".//Filer/EIN"
+        ".//Filer/EIN",
     ],
     "filer_name": [
         ".//irs:Filer/irs:BusinessName/irs:BusinessNameLine1Txt",
-        ".//Filer/BusinessName/BusinessNameLine1Txt"
+        ".//Filer/BusinessName/BusinessNameLine1Txt",
     ],
     "receipt": [
         ".//irs:IRS990/irs:GrossReceiptsAmt",
@@ -21,58 +21,58 @@ XPATHS_990 = {
         ".//irs:IRS990/irs:TotalRevenueAmt",
         ".//irs:TotalRevenueAmt",
         ".//irs:IRS990/irs:CYTotalRevenueAmt",
-        ".//irs:CYTotalRevenueAmt"
+        ".//irs:CYTotalRevenueAmt",
     ],
     "govt_grants": [
         ".//irs:IRS990/irs:GovernmentGrantsAmt",
         ".//irs:GovernmentGrantsAmt",
         ".//irs:IRS990/irs:GovtContriGrntAmt",
         ".//irs:GovtContriGrntAmt",
-        ".//irs:ContributionsAndGrantsAmt"
+        ".//irs:ContributionsAndGrantsAmt",
     ],
     "contributions": [
         ".//irs:IRS990/irs:AllOtherContributionsAmt",
         ".//irs:AllOtherContributionsAmt",
         ".//irs:IRS990/irs:TotalContributionsAmt",
-        ".//irs:TotalContributionsAmt"
+        ".//irs:TotalContributionsAmt",
     ],
     "total_exp": [
         ".//irs:IRS990/irs:TotalFunctionalExpensesGrp/irs:TotalAmt",
         ".//irs:TotalFunctionalExpensesGrp/irs:TotalAmt",
         ".//irs:IRS990/irs:TotalExpensesAmt",
-        ".//irs:TotalExpensesAmt"
+        ".//irs:TotalExpensesAmt",
     ],
     "prog_exp": [
         ".//irs:IRS990/irs:TotalProgramServiceExpensesAmt",
         ".//irs:TotalProgramServiceExpensesAmt",
-        ".//irs:ProgramServiceExpensesAmt"
+        ".//irs:ProgramServiceExpensesAmt",
     ],
     "travel": [
         ".//irs:IRS990/irs:TravelGrp/irs:TotalAmt",
         ".//irs:TravelGrp/irs:TotalAmt",
-        ".//irs:TravelExpensesAmt"
+        ".//irs:TravelExpensesAmt",
     ],
     "conferences": [
         ".//irs:IRS990/irs:ConferencesMeetingsGrp/irs:TotalAmt",
-        ".//irs:ConferencesMeetingsGrp/irs:TotalAmt"
+        ".//irs:ConferencesMeetingsGrp/irs:TotalAmt",
     ],
     "officer_comp_elements": [
         ".//irs:IRS990/irs:Form990PartVIISectionAGrp",
         ".//irs:Form990PartVIISectionAGrp",
         ".//irs:IRS990/irs:OfficerDirectorTrusteeEmplGrp",
-        ".//irs:OfficerDirectorTrusteeEmplGrp"
+        ".//irs:OfficerDirectorTrusteeEmplGrp",
     ],
     "officer_comp_value": [
         "irs:ReportableCompFromOrgAmt",
         "ReportableCompFromOrgAmt",
         "irs:CompensationAmt",
-        "CompensationAmt"
+        "CompensationAmt",
     ],
     "officer_comp": [
         ".//irs:IRS990/irs:Form990PartVIISectionAGrp/irs:ReportableCompFromOrgAmt",
         ".//irs:Form990PartVIISectionAGrp/irs:ReportableCompFromOrgAmt",
         ".//irs:IRS990/irs:OfficerDirectorTrusteeEmplGrp/irs:CompensationAmt",
-        ".//irs:OfficerDirectorTrusteeEmplGrp/irs:CompensationAmt"
+        ".//irs:OfficerDirectorTrusteeEmplGrp/irs:CompensationAmt",
     ],
     "grants_to_others": [
         ".//irs:ReturnData/irs:IRS990ScheduleF/irs:GrantsToOrgOutsideUSGrp/irs:CashGrantAmt",
@@ -80,53 +80,40 @@ XPATHS_990 = {
         ".//irs:ReturnData/irs:IRS990ScheduleF/irs:GrantsToOrgsOutsideUS/irs:CashGrantAmt",
         ".//irs:ReturnData/irs:IRS990ScheduleF/irs:ForeignIndividualsGrantsGrp/irs:CashGrantAmt",
         ".//irs:ReturnData/irs:IRS990ScheduleI/irs:RecipientTable/irs:CashGrantAmt",
-        ".//irs:ReturnData/irs:IRS990ScheduleI/irs:GrantsOtherAsstToIndivInUSGrp/irs:CashGrantAmt"
+        ".//irs:ReturnData/irs:IRS990ScheduleI/irs:GrantsOtherAsstToIndivInUSGrp/irs:CashGrantAmt",
     ],
     "grant_elements_f": [
         ".//irs:ReturnData/irs:IRS990ScheduleF",
-        ".//irs:ReturnData/irs:IRS990ScheduleF"
     ],
     "grant_sub_elements_f": [
         ".//irs:GrantsToOrgOutsideUSGrp",
-        ".//irs:GrantsToOrgOutsideUSGrp",
         ".//irs:GrantsToOrganizationsOutsideUS",
-        ".//irs:GrantsToOrganizationsOutsideUS",
-        ".//irs:GrantsToOrgsOutsideUS",
         ".//irs:GrantsToOrgsOutsideUS",
         ".//irs:ForeignIndividualsGrantsGrp",
-        ".//irs:ForeignIndividualsGrantsGrp"
     ],
     "grant_elements_i": [
         ".//irs:ReturnData/irs:IRS990ScheduleI",
-        ".//irs:ReturnData/irs:IRS990ScheduleI"
     ],
     "grant_sub_elements_i": [
         ".//irs:RecipientTable",
-        ".//irs:RecipientTable",
         ".//irs:GrantsOtherAsstToIndivInUSGrp",
-        ".//irs:GrantsOtherAsstToIndivInUSGrp"
     ],
     "grant_value": [
         "irs:CashGrantAmt",
-        "irs:CashGrantAmt"
     ],
     "foreign_expenses": [
         ".//irs:ReturnData/irs:IRS990ScheduleF/irs:StmtOfActyOutsdUSGrp/irs:RegionTotalExpendituresAmt",
-        ".//irs:ReturnData/irs:IRS990ScheduleF/irs:AccountActivitiesOutsideUSGrp/irs:RegionTotalExpendituresAmt"
+        ".//irs:ReturnData/irs:IRS990ScheduleF/irs:AccountActivitiesOutsideUSGrp/irs:RegionTotalExpendituresAmt",
     ],
     "foreign_exp_elements": [
         ".//irs:ReturnData/irs:IRS990ScheduleF",
-        ".//irs:ReturnData/irs:IRS990ScheduleF"
     ],
     "foreign_exp_sub_elements": [
         ".//irs:StmtOfActyOutsdUSGrp",
-        ".//irs:StmtOfActyOutsdUSGrp",
         ".//irs:AccountActivitiesOutsideUSGrp",
-        ".//irs:AccountActivitiesOutsideUSGrp"
     ],
     "foreign_exp_value": [
         "irs:RegionTotalExpendituresAmt",
-        "irs:RegionTotalExpendituresAmt"
     ],
     "org_type": [
         ".//irs:IRS990/irs:Organization501cInd",
@@ -141,18 +128,18 @@ XPATHS_990 = {
         ".//irs:Organization501c6Ind",
         ".//irs:IRS990/irs:Organization4947a1NotPFInd",
         ".//irs:Organization4947a1NotPFInd",
-        ".//irs:TaxExemptOrganizationInd"
+        ".//irs:TaxExemptOrganizationInd",
     ],
     "foreign_office": [
         ".//irs:IRS990/irs:ForeignOfficeInd",
         ".//irs:ForeignOfficeInd",
         ".//irs:ForeignOfficeCountryCd",
-        ".//irs:ForeignActivitiesInd"
+        ".//irs:ForeignActivitiesInd",
     ],
     "total_assets": [
         ".//irs:IRS990/irs:TotalAssetsEOYAmt",
         ".//irs:TotalAssetsEOYAmt",
         ".//irs:IRS990/irs:TotalAssetsAmt",
-        ".//irs:TotalAssetsAmt"
-    ]
+        ".//irs:TotalAssetsAmt",
+    ],
 }
