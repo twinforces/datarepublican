45af2df27f615315002fd10f830e1e233a53b113
Pierce Wetter <pierce@twinforces.com>
2025-06-12 08:00:46 -0700

    better directories.

--- 990tools/doEverything.sh
+++ 990tools/doEverything.sh
@@ -7,10 +7,10 @@ python download_IRS_990_zips.py 2017 2025 --test /Volumes/Data/irs_zips
 #cd /Volumes/Data/irs_zips; python recompress_irs_zips
 
 python extract_charities.py 2017 2025 --quiet --input-dir /Volumes/Data/irs_zips --output-dir /Volumes/Data/tsvs
-python analyze_charities.py --start-year 2017 --stop-year 2025 --input-dir /Volumes/Data/tsvs --output-dir /Volumes/Data/tsvs 
-python get_latest.py 2017 2025 --zip-dir /Volumes/Data/irs_zips --source-dir /Volumes/Data/tsvs --output-dir ./latest
-cp latest/*.tsv /Volumes/Data/tsvs
-sh combine_grants.sh /Volumes/Data/tsvs/grants_latest.tsv /Volumes/Data/tsvs/grants_combined.tsv
-python extract_addresses_and_grants.py 2017 2025 --zip-dir /Volumes/Data/irs_zips --source-dir /Volumes/Data/tsvs --output-dir ./latest --quiet
+python analyze_charities.py --start-year 2017 --stop-year 2025 --input-dir /Volumes/Data/tsvs --output-dir /Volumes/Data/atsvs 
+python get_latest.py 2017 2025 --zip-dir /Volumes/Data/irs_zips --source-dir /Volumes/Data/atsvs --output-dir ./latest
+cp latest/*.tsv /Volumes/Data/atsvs
+sh combine_grants.sh /Volumes/Data/atsvs/grants_latest.tsv /Volumes/Data/atsvs/agrants_combined.tsv
+python extract_addresses_and_grants.py 2017 2025 --zip-dir /Volumes/Data/irs_zips --source-dir /Volumes/Data/atsvs --output-dir ./latest --quiet
 sh combine_grants.sh ./latest/inferred_grants.tsv ./latest/inferred_combined.tsv
 
