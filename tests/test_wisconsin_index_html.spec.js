397841d152daaed2d18bb38436456100f28796da
Pierce Wetter <pierce@twinforces.com>
2025-07-12 12:10:07 -0700

    Updated tests.

--- tests/test_wisconsin_index_html.spec.js
+++ tests/test_wisconsin_index_html.spec.js
@@ -1,9 +1,9 @@
 const { test, expect } = require('@playwright/test');
 
-
 test('wisconsin_index_html loads correctly', async ({ page }) => {
   const response = await page.goto(`${process.env.HOST || 'http://localhost:4000'}/wisconsin/index.html`);
   expect(response.status()).toBe(200);
-  await expect(page).toHaveTitle(/.+/);
-  // Add more assertions here (e.g., await expect(page.locator('h1')).toBeVisible());
+  await page.waitForFunction('document.title !== ""');
+  await expect(page).toHaveTitle('map');
+  // Add more assertions here
 });
